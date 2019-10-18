8f1736f81221f514e5420c33572abf2ef007f0c6
xavid.ramirez@apchomehealth.onmicrosoft.com <xavid.ramirez@apchomehealth.onmicrosoft.com>
2019-10-18 16:39:33 -0500

    Add project files.

---
+++ APC.GraphQL.Poco/Models/TableProperties.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace APC.GraphQL.Poco.Models
+{
+
+	public class TableProperties
+	{
+		public string ColumnName { get; set; }
+		public string DataType { get; set; }
+		public int MaxLength { get; set; }
+		public bool IsPrimaryKey { get; set; }
+
+		public Type GetColumnType()
+		{
+			return Type.GetType(DataType);
+		}
+	}
+
+}
